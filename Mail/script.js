335b5f6ec4778b33468e2ed24fdbd002bf8f7761
Damiano Celesti <damiano.celesti@gmail.com>
2024-12-20 14:39:37 +0100

    add es email

--- Mail/script.js
+++ Mail/script.js
@@ -3,3 +3,35 @@
 // Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
 // Si può fare? Certo che si basta ragionare un po’.
 // Nota: Non è necessario provvedere alla validazione delle email
+
+// lista di email invitati
+
+const email = ['gino@gmail.it', 'paolo@gmail.it', 'luca@gmail.it', 'giacomo@gmail.it','luna@gmail.it'];
+
+// chiedi all utente email
+
+const emailInput = prompt('inserisci email');
+
+
+// variabile per vedere se ce
+
+let emailDaConf = false;
+
+
+// ciclo for per vedere tutte le email e se sono uguali
+
+for (let i = 0; i < email.length; i++) {
+    if (emailDaConf[i] === emailInput) {
+        emailDaConf = true;
+        break;
+    }
+
+}
+
+
+if (emailDaConf) {
+    console.log('email registrata accedi');
+
+} else {
+    console.log('non puo ipartecipare')
+}
